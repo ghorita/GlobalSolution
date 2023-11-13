d025f58a939ca0dc9205143a74a522b41006c2fc
ghorit <rm94860@fiap.com.br>
2023-11-13 15:38:45 -0300

    Criação do Projeto

---
+++ GS/App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import TelaCadastro from './src/pages/Cadastro/index';
+
+export default function App() {
+  return (
+    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+      <Text>Cadastro</Text>
+    </View>
+  );
+}
+
+
+
+
---
+++ GS/src/pages/Cadastro/index.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+
+export default function TelaCadastro() {
+    return(
+        <View>
+            <Text>Tela Cadastro</Text>
+        </View>
+    );
+}
